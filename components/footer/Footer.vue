1845ef1c31c34ab600339de0b8a66a91d3925aa0
omar osama <119760793+omarosamaali@users.noreply.github.com>
2024-10-19 16:53:01 +0300

    Update Footer.vue

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -5,7 +5,7 @@
                 <div class="grid grid-cols-1 gap-8 sm:gap-6 sm:grid-cols-2 md:grid-cols-4 w-full">
 
                     <div class="mb-6 md:mb-0">
-                        <a href="https://flowbite.com/" class="flex items-center">
+                        <a href=" " class="flex items-center">
                             <img src="../../public/img/logo.png" class="h-[41px] w-[204px] me-3" alt="Logo" />
                         </a>
                     </div>
@@ -13,18 +13,18 @@
                         <h2 class="mb-6 text-[18px] font-bold text-[#6E7491]">{{ $t('About') }}</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('About GooseFlight') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('About GooseFlight') }}</a>
                             </li>
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('How it works') }}</a>
-                            </li>
-
-                            <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Blog') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('How it works') }}</a>
                             </li>
 
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Forum') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Blog') }}</a>
+                            </li>
+
+                            <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
+                                <a href=" " class="hover:underline">{{ $t('Forum') }}</a>
                             </li>
                         </ul>
                     </div>
@@ -32,26 +32,26 @@
                         <h2 class="mb-6 text-[18px] font-bold text-[#6E7491]">{{ $t('Support') }}</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Help Center') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Help Center') }}</a>
                             </li>
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Contact us') }}</a>
-                            </li>
-
-                            <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Privacy policy') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Contact us') }}</a>
                             </li>
 
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Terms of service') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Privacy policy') }}</a>
                             </li>
 
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Trust and safety') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Terms of service') }}</a>
                             </li>
 
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Accessibility') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Trust and safety') }}</a>
+                            </li>
+
+                            <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
+                                <a href=" " class="hover:underline">{{ $t('Accessibility') }}</a>
                             </li>
                         </ul>
                     </div>
@@ -60,14 +60,14 @@
                         <h2 class="mb-6 text-[18px] font-bold text-[#6E7491]">{{ $t('Get the app') }}</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Tripma for Android') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Tripma for Android') }}</a>
                             </li>
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Tripma for iOS') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Tripma for iOS') }}</a>
                             </li>
 
                             <li class="mb-4 text-[16px] text-[#7C8DB0] font-normal">
-                                <a href="https://flowbite.com/" class="hover:underline">{{ $t('Mobile site') }}</a>
+                                <a href=" " class="hover:underline">{{ $t('Mobile site') }}</a>
                             </li>
 
                             <li class="mb-4">
